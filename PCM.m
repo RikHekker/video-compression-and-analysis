2549a985bd1c7abcaa51f7b605c4f2d147f462e2
Hekker <f.e.j.hekker@student.tue.nl>
2019-03-13 14:24:00 +0100

    added a quantizer

--- PCM.m
+++ PCM.m
@@ -1,1 +1,15 @@
-lena=imread('lena.pmg');
+clear all; close all;
+%hyper parameters
+stepsize=10;
+lena=imread('lena.pgm'); %load the test picture
+
+%odd uniform quantizer
+for i=1:numel(lena) %loop over all the elements in the lena matrix
+    qindex=idivide(lena(i),stepsize); %rounded to the nearest interger
+    lena(i)=qindex*stepsize;
+end
+
+%show the new picture
+lena=mat2gray(lena);
+figure
+imshow(lena)
