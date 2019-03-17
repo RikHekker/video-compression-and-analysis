aa156fdec8b0ac15c3d88c1130bb60bb27f11aab
Hekker <f.e.j.hekker@student.tue.nl>
2019-03-17 14:44:32 +0100

    renamed variables for adaptability and added a picture for testing

--- PCM.m
+++ PCM.m
@@ -5,22 +5,25 @@ stepsize=255/K;
 
 %load the test picture
 lena=double(imread('lena.pgm')); 
+peppers=double(imread('peppers.pgm')); 
 original=lena;
 
-vector=lena(:);
+vector=original(:);
 sigma_x=var(vector);
 sigma_q=0;
 pd=fitdist(vector,'normal');
 
 %odd uniform quantizer
-for i=1:numel(lena) %loop over all the elements in the lena matrix
-    qindex=idivide(lena(i),int8(stepsize)); %rounded to the nearest interger
+for i=1:numel(original) %loop over all the elements in the lena matrix
+    qindex=idivide(original(i),int8(stepsize)); %rounded to the nearest interger
     if qindex>K
         qindex=K;
     end
-    lena(i)=qindex*stepsize;
+    new(i)=qindex*stepsize;
 end
+new=reshape(new,[512,512])
 
+%SNR
 for x=0:255
     q=floor(x/stepsize+0.5)*stepsize;
     sigma_q= sigma_q+(x-q).^2*pdf(pd,x);
@@ -28,7 +31,7 @@ end
 SNR=10*log10(sigma_x^2/sigma_q^2);
 
 %show the new picture
-lena=mat2gray(lena);
+new=mat2gray(new);
 figure
-imshow(lena)
+imshow(new)
 
